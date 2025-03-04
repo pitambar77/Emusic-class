5feb3fff749a47c621c6e55c975ea09e0dc4a193
pitambar77 <pitambermajhi33@gmail.com>
2025-03-05 01:35:17 +0530

    six commit

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -38,15 +38,15 @@ const page = () => {
                     />
                   </CardItem>
                   <div className="flex justify-between items-center mt-20">
-                    <CardItem
-                      translateZ={20}
-                      as={Link}
-                      href="https://twitter.com/mannupaaji"
-                      target="__blank"
-                      className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
-                    >
-                      Try now →
-                    </CardItem>
+                  <Link href="https://twitter.com/mannupaaji" target="_blank" rel="noopener noreferrer">
+                  < CardItem
+                    translateZ={20}
+                    className="px-4 py-2 rounded-xl text-xs font-normal dark:text-white"
+                  >
+                    Try now →
+                  </CardItem>
+                  </Link>
+
                     <CardItem
                       translateZ={20}
                       as="button"
